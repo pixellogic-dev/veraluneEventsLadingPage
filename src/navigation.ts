b3550139e5a2e134270caf5688d13de41f56ab1c
Gabriel Crooke <gabrielcrooke@gmail.com>
2026-02-05 22:13:05 -0400

    🚀 Completing copy for Experiences section

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,4 +1,4 @@
-import { getPermalink, getAsset } from "./utils/permalinks";
+import { getPermalink } from "./utils/permalinks";
 
 export const headerData = {
   links: [
@@ -101,15 +101,23 @@ export const footerData = {
     { text: "Privacy Policy", href: getPermalink("/privacy") },
   ],
   socialLinks: [
-    { ariaLabel: "X", icon: "tabler:brand-x", href: "#" },
-    { ariaLabel: "Instagram", icon: "tabler:brand-instagram", href: "#" },
-    { ariaLabel: "Facebook", icon: "tabler:brand-facebook", href: "#" },
-    { ariaLabel: "RSS", icon: "tabler:rss", href: getAsset("/rss.xml") },
     {
+      ariaLabel: "WhatsApp",
+      icon: "tabler:brand-whatsapp",
+      href: "https://www.instagram.com/veralune_events/",
+    },
+    {
+      ariaLabel: "Instagram",
+      icon: "tabler:brand-instagram",
+      href: "https://www.instagram.com/veralune_events/",
+    },
+    /*{ ariaLabel: "Facebook", icon: "tabler:brand-facebook", href: "#" },*/
+    /*{ ariaLabel: "RSS", icon: "tabler:rss", href: getAsset("/rss.xml") },*/
+    /*{
       ariaLabel: "Github",
       icon: "tabler:brand-github",
       href: "https://github.com/arthelokyo/astrowind",
-    },
+    },*/
   ],
   footNote: `
   © ${new Date().getFullYear()} Crafted by
